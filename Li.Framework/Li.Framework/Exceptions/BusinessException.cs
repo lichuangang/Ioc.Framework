cb5a2fc6ed7817c2aac83061596b6109698858b6
lichuangang <lcgang1988@163.com>
2017-07-20 14:56:54 +0800

    3

---
+++ Li.Framework/Li.Framework/Exceptions/BusinessException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Li.Framework.Exceptions
+{
+    /* ==============================================================================
+     * 描述：BusinessException
+     * 创建人：李传刚 2017/7/20 14:54:32
+     * ==============================================================================
+     */
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message, int? code = null)
+            : base(message)
+        {
+            Code = code;
+        }
+
+        public int? Code { get; set; }
+    }
+}
